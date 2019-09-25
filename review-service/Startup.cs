e580b5f5fde60fcd727c0e4ccbf04a8a20509d1d
jeremiahbuckley <jeremiah.buckley@gmail.com>
2019-09-25 15:09:27 -0400

    added environmental variables and logging for review-service

--- review-service/Startup.cs
+++ review-service/Startup.cs
@@ -11,18 +11,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using Microsoft.Extensions.Logging;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 namespace ReviewService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
         {
             Configuration = configuration;
+            Log = logger;
         }
 
         public IConfiguration Configuration { get; }
+        public ILogger<Startup> Log { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,8 +40,7 @@ namespace ReviewService
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            var optionsBuilder = new DbContextOptionsBuilder<ReviewContext>();
-            optionsBuilder.UseNpgsql("Host=localhost;Database=review;Username=swarm;Password=password");
+            var optionsBuilder = ConfigurePostgreSQLConnection();
             services.AddSingleton<ReviewContext>(_ => new ReviewContext(optionsBuilder.Options));
             services.AddScoped<IReviewSvc, ReviewSvc>();
         }
@@ -61,6 +64,32 @@ namespace ReviewService
             app.UseCookiePolicy();
 
             app.UseMvc();
+        }
+        private DbContextOptionsBuilder<ReviewContext> ConfigurePostgreSQLConnection() {
+            string db_jndi = Environment.GetEnvironmentVariable("REVIEW_DB_JNDI"); //in coolstuff-store this is hard-coded to 127.0.0.1, no env variable";
+            string db_username = Environment.GetEnvironmentVariable("REVIEW_DB_USERNAME");
+            string db_password = Environment.GetEnvironmentVariable("REVIEW_DB_PASSWORD");
+            string db_database = Environment.GetEnvironmentVariable("REVIEW_DB_DATABASE");
+
+            string db_host = db_jndi;
+            if (string.IsNullOrWhiteSpace(db_jndi)) {
+                db_host = "127.0.0.1";
+                Log.LogInformation("DB_JNDI environmental variable is not set, using {0}", db_host);
+            }
+
+            if(string.IsNullOrWhiteSpace(db_database)) {
+                db_database = "review";
+                Log.LogInformation("DB_DATABASE environment variable is empty, using : {0}", db_database);
+            }
+
+            if (string.IsNullOrWhiteSpace(db_username) || string.IsNullOrWhiteSpace(db_password)) {
+                db_username = "swarm";
+                db_password = "password";
+                Log.LogInformation("DB_USERNAME or DB_PASSWORD environment variable is empty, using User:{0} and Password:[redacted]", db_username);
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<ReviewContext>();
+            return optionsBuilder.UseNpgsql(string.Format("Host={0};Database={1};Username={2};Password={3}", db_host, db_database, db_username, db_password));
         }
     }
 }
